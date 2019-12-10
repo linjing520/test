82d7d04032f093259d3e80965414f747121ab347
Linjing <123@qq.com>
2019-12-10 09:22:53 +0800

    创建一个工程

---
+++ git_test_perject/src/com/linjing/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.linjing.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+    }
+}
